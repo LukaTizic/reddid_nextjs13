f8d0bfa53fc59b79003cb0db13ab6fb9fb4f78cb
aluca94@gmail.com <alutza94@gmail.com>
2024-01-05 12:12:08 +0100

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,17 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
 import "./globals.css";
+import { Inter, Space_Grotesk } from "next/font/google";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "FlowDev LT",
+  description:
+    "A community-driven platform for asking and answering programming questions! Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: {
+    icon: "",
+  },
+};
 
 export default function RootLayout({
   children,
