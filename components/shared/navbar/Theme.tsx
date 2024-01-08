610dc9e9a6eec089b47129d04c419f0943d98fc8
aluca94@gmail.com <alutza94@gmail.com>
2024-01-08 17:43:28 +0100

    add MenuBar component

---
+++ components/shared/navbar/Theme.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useTheme } from "@/context/ThemeProvider";
+
+const Theme = () => {
+  const { mode, setMode } = useThem();
+  return <div>Theme</div>;
+};
+
+export default Theme;
