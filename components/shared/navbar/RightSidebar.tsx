9d06cb279d50d23f8a18d45bfc5f31fc57324ef6
aluca94@gmail.com <alutza94@gmail.com>
2024-01-10 13:59:22 +0100

    add TopQuestions

--- components/shared/navbar/RightSidebar.tsx
+++ components/shared/navbar/RightSidebar.tsx
@@ -1,25 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const dummy = [
   {
     _id: 1,
-    title: "velit egestas dui id ornare arcu odio ut",
+    title: "Velit egestas dui id ornare arcu odio ut",
   },
   {
     _id: 2,
-    title: "velit egestas dui id ornare arcu odio ut",
+    title: "Velit egestas dui id ornare arcu odio ut",
   },
   {
     _id: 3,
-    title: "velit egestas dui id ornare arcu odio ut",
+    title: "Velit egestas dui id ornare arcu odio ut",
   },
   {
     _id: 4,
-    title: "velit egestas dui id ornare arcu odio ut",
+    title: "Velit egestas dui id ornare arcu odio ut",
   },
   {
     _id: 5,
-    title: "velit egestas dui id ornare arcu odio ut",
+    title: "Velit egestas dui id ornare arcu odio ut",
   },
 ];
 
@@ -28,7 +30,24 @@ const RightSidebar = () => {
     <section className='background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen flex-col  overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden w-[350px]'>
       <div>
         <h3 className='h3-bold text-dark200_light900'>Top Questions</h3>
-        <div className='mt-7 flex w-full flex-col gap-[30px]'></div>
+        <div className='mt-7 flex w-full flex-col gap-[30px]'>
+          {dummy.map((item) => (
+            <Link
+              href={`/questions/${item._id}`}
+              key={item._id}
+              className='flex cursor-pointer items-center justify-between gap-7'
+            >
+              <p className='body-medium text-dark500_light700 '>{item.title}</p>
+              <Image
+                src='/assets/icons/chevron-right.svg'
+                alt='arrow'
+                width={20}
+                height={20}
+                className='invert colors'
+              />
+            </Link>
+          ))}
+        </div>
       </div>
       <div className='mt-16'>test</div>
     </section>
