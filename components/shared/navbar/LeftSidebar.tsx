813f61d017d7a907cc8a110c29ae8e3c5dac7d85
aluca94@gmail.com <alutza94@gmail.com>
2024-01-10 13:36:24 +0100

    add responsiveness

--- components/shared/navbar/LeftSidebar.tsx
+++ components/shared/navbar/LeftSidebar.tsx
@@ -1,33 +1,34 @@
 "use client";
+
 import { sidebarLinks } from "@/constants";
-import Link from "next/link";
 import Image from "next/image";
-import React from "react";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { SignedOut } from "@clerk/nextjs";
 
 const LeftSidebar = () => {
   const pathname = usePathname();
+
   return (
-    <section className='background-light900_dark200 light-border sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-30 dark:shadow-none max-sm:hidden lg:w-[266px] custom-scrollbar'>
-      <div className='flex flex-1 flex-col gap-62 '>
+    <section className='background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]'>
+      <div className='flex flex-1 flex-col gap-6'>
         {sidebarLinks.map((item) => {
           const isActive =
             (pathname.includes(item.route) && item.route.length > 1) ||
             pathname === item.route;
 
-          //TODO
+          // TODO
 
           return (
             <Link
-              key={item.route}
               href={item.route}
+              key={item.label}
               className={`${
                 isActive
                   ? "primary-gradient rounded-lg text-light-900"
                   : "text-dark300_light900"
-              } flex items-center justify-start gap-4 bg-transparent p-4`}
+              }  flex items-center justify-start gap-4 bg-transparent p-4`}
             >
               <Image
                 src={item.imgURL}
@@ -38,8 +39,8 @@ const LeftSidebar = () => {
               />
               <p
                 className={`${
-                  isActive ? "base-bold border-b-2" : "base-medium"
-                }`}
+                  isActive ? "base-bold" : "base-medium"
+                } max-lg:hidden`}
               >
                 {item.label}
               </p>
@@ -47,17 +48,34 @@ const LeftSidebar = () => {
           );
         })}
       </div>
+
       <SignedOut>
         <div className='flex flex-col gap-3'>
           <Link href='/sign-in'>
             <Button className='small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none'>
-              <span className='primary-text-gradient'>Log In</span>
+              <Image
+                src='/assets/icons/account.svg'
+                alt='login'
+                width={20}
+                height={20}
+                className='invert-colors lg:hidden'
+              />
+              <span className='primary-text-gradient max-lg:hidden'>
+                Log In
+              </span>
             </Button>
           </Link>
 
           <Link href='/sign-up'>
             <Button className='small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none'>
-              Sign Up
+              <Image
+                src='/assets/icons/sign-up.svg'
+                alt='sign up'
+                width={20}
+                height={20}
+                className='invert-colors lg:hidden'
+              />
+              <span className='max-lg:hidden'>Sign up</span>
             </Button>
           </Link>
         </div>
