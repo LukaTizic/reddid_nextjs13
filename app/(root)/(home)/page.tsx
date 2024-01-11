90e9df9207b7bbf0f2111ac56188edde624c4426
aluca94@gmail.com <alutza94@gmail.com>
2024-01-11 18:38:15 +0100

    implement hero skeleton

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,18 @@
+import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Home() {
-  return <div className='h-screen'></div>;
+  return (
+    <>
+      <div>
+        <h1 className='h1-bold text-dark100_light900'>All Questions</h1>
+      </div>
+      <Link href='/ask-question'>
+        <Button className='primary-gradient min-h-[46px] px-4 py-3 !text-light-900'>
+          Ask a Question
+        </Button>
+      </Link>
+    </>
+  );
 }
