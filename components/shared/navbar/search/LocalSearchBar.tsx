6b11b19aee7b32a8f984801c6cdc4440292c3316
aluca94@gmail.com <alutza94@gmail.com>
2024-01-11 18:54:05 +0100

    add UI for LocalSearchBar

--- components/shared/navbar/search/LocalSearchBar.tsx
+++ components/shared/navbar/search/LocalSearchBar.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import Image from "next/image";
 import React from "react";
 
 interface CustomInputProps {
@@ -5,7 +9,7 @@ interface CustomInputProps {
   iconPosition: string;
   imgSrc: string;
   placeholder: string;
-  otherClasses: string;
+  otherClasses?: string;
 }
 
 const LocalSearchBar = ({
@@ -15,7 +19,37 @@ const LocalSearchBar = ({
   placeholder,
   otherClasses,
 }: CustomInputProps) => {
-  return <div>LocalSearchBar</div>;
+  return (
+    <div
+      className={`background-light800_darkgradient flex min-h-[56px] grow items-center gap-5 rounded-[10px] px-4 ${otherClasses} `}
+    >
+      {iconPosition === "left" && (
+        <Image
+          src={imgSrc}
+          width={24}
+          height={24}
+          alt='Search Icon'
+          className='cursor-pointer'
+        />
+      )}
+      <Input
+        type='text'
+        placeholder={placeholder}
+        value=''
+        onChange={() => {}}
+        className='paragraph-regular no-focus placeholder background-light800_darkgradient border-none shadow-none outline-none'
+      />
+      {iconPosition === "right" && (
+        <Image
+          src={imgSrc}
+          width={24}
+          height={24}
+          alt='Search Icon'
+          className='cursor-pointer'
+        />
+      )}
+    </div>
+  );
 };
 
 export default LocalSearchBar;
