b3d4751820d1323e5f92bb6e47bea4295508c543
aluca94@gmail.com <alutza94@gmail.com>
2024-01-11 19:15:25 +0100

    implement Filter UI

--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -1,11 +1,14 @@
+"use client";
 import React from "react";
 import {
   Select,
   SelectContent,
+  SelectGroup,
   SelectItem,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Item } from "@radix-ui/react-menubar";
 
 interface Props {
   filters: { name: string; value: string }[];
@@ -17,13 +20,21 @@ const Filter = ({ filters, otherClasses, containerClasses }: Props) => {
   return (
     <div className={`relative ${containerClasses}`}>
       <Select>
-        <SelectTrigger className='w-[180px]'>
-          <SelectValue placeholder='Theme' />
+        <SelectTrigger
+          className={`${otherClasses} body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5`}
+        >
+          <div className='line-clamp-1 flex-1 text-left'>
+            <SelectValue placeholder='Select a Filter' />
+          </div>
         </SelectTrigger>
         <SelectContent>
-          <SelectItem value='light'>Light</SelectItem>
-          <SelectItem value='dark'>Dark</SelectItem>
-          <SelectItem value='system'>System</SelectItem>
+          <SelectGroup>
+            {filters.map((filter) => (
+              <SelectItem key={filter.value} value={filter.value}>
+                {filter.name}
+              </SelectItem>
+            ))}
+          </SelectGroup>
         </SelectContent>
       </Select>
     </div>
