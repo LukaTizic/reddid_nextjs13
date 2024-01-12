caaadc4d71b772914cd4fd750725984c2edd84a8
aluca94@gmail.com <alutza94@gmail.com>
2024-01-12 13:02:34 +0100

    made NoResults component dynamic #1

--- components/shared/NoResult.tsx
+++ components/shared/NoResult.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "../ui/button";
+import link from "next/link";
 
 interface Props {
   title: string;
@@ -27,17 +28,13 @@ const NoResult = ({ title, description, link, linkTitle }: Props) => {
         height={200}
         className='hidden object-contain dark:flex'
       />
-      <h2 className='h2-bold text-dark200_light900 mt-8'>
-        There's no question to show
-      </h2>
+      <h2 className='h2-bold text-dark200_light900 mt-8'>{title}</h2>
       <p className='body-regular text-dark500_light700 my-3.5 max-w-md text-center'>
-        Be the first to break the silence! Ask a Question and kickstart the
-        discussion.Our query could be the next big thing others learn from! Get
-        involved and help!
+        {description}
       </p>
-      <Link href='/'>
+      <Link href={link}>
         <Button className='paragraph-medium font-bold mt-5 min-h-[46px] rounded-lg bg-primary-500 px-4 py-3 text-light-900 hover:bg-white hover:text-orange-500 border dark:border-orange-500 hover:border-orange-500'>
-          Ask a Question
+          {linkTitle}
         </Button>
       </Link>
     </div>
