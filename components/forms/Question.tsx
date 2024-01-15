dbb0a83d62c40de169cc60e6cb6e4f3d214dfde6
aluca94@gmail.com <alutza94@gmail.com>
2024-01-15 19:00:02 +0100

    implement form/input

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,8 +1,110 @@
 "use client";
-import React from "react";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { QuestionsSchema } from "@/lib/validations";
 
 const Question = () => {
-  return <div>Question</div>;
+  const form = useForm<z.infer<typeof QuestionsSchema>>({
+    resolver: zodResolver(QuestionsSchema),
+    defaultValues: {
+      title: "",
+      explanation: "",
+      tags: [],
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof QuestionsSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='flex w-full flex-col gap-10'
+      >
+        <FormField
+          control={form.control}
+          name='title'
+          render={({ field }) => (
+            <FormItem className='flex w-full flex-col'>
+              <FormLabel className='paragraph-semibold text-dark400_light800'>
+                Question title <span className='text-primary-500'>*</span>
+              </FormLabel>
+              <FormControl className='mt-3.5'>
+                <Input
+                  className='no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border'
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription className='body-regular mt-3.5 text-light-500'>
+                Be specific and precise with your problem.
+              </FormDescription>
+              <FormMessage className='text-red-500' />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='explanation'
+          render={({ field }) => (
+            <FormItem className='flex w-full flex-col gap-3'>
+              <FormLabel className='paragraph-semibold text-dark400_light800'>
+                Detailed explanation of your problem{" "}
+                <span className='text-primary-500'>*</span>
+              </FormLabel>
+              {/* TODO */}
+              <FormDescription className='body-regular mt-3.5 text-light-500'>
+                Introduce the problem and expand on what you put in the title.
+                Make it minimum 20 characters.
+              </FormDescription>
+              <FormMessage className='text-red-500' />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='tags'
+          render={({ field }) => (
+            <FormItem className='flex w-full flex-col'>
+              <FormLabel className='paragraph-semibold text-dark400_light800'>
+                Tags <span className='text-primary-500'>*</span>
+              </FormLabel>
+              <FormControl className='mt-3.5'>
+                <Input
+                  className='no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border'
+                  placeholder='Add tags...'
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription className='body-regular mt-3.5 text-light-500'>
+                Add up to 3 tags to describe what your question is about. You
+                need to press enter to add a tag.{" "}
+              </FormDescription>
+              <FormMessage className='text-red-500' />
+            </FormItem>
+          )}
+        />
+        <Button type='submit'>Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default Question;
