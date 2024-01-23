a95ac7c0b6f93a7f9757358a482fc5dcab52aee4
aluca94@gmail.com <alutza94@gmail.com>
2024-01-23 19:31:01 +0100

    add skeleton to tag.actions

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -1,11 +1,21 @@
 "use server";
 
+import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
 import { GetTopInteractedTagsParams } from "./shared.types";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
     connectToDatabase();
+
+    const { userId, limit = 3 } = params;
+
+    const user = await User.findById(userId);
+    if (!user) throw new Error("User not found");
+
+    //Interactions
+
+    return ["tag1", "tag2", "tag3"];
   } catch (error) {
     console.log(error);
     throw error;
