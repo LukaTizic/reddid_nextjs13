accf29ac6d7c42521f1c89a6f1f70eb84906587a
aluca94@gmail.com <alutza94@gmail.com>
2024-01-23 18:22:12 +0100

    add input / filters

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import Filter from "@/components/shared/Filter";
+import LocalSearchBar from "@/components/shared/navbar/search/LocalSearchBar";
+import { UserFilters } from "@/constants/filters";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <h1 className='h1-bold text-dark100_light900'>All Users</h1>
+
+      <div className='mt-11 flex justify-between gap-5 max-sm:flex-col items-center'>
+        <LocalSearchBar
+          route='/community'
+          iconPosition='left'
+          imgSrc='/assets/icons/search.svg'
+          placeholder='Search for amazing minds'
+          otherClasses='flex-1'
+        />
+        <Filter
+          filters={UserFilters}
+          otherClasses='min-h-[56px] sm:min-w-[170px]'
+          containerClasses='hidden max-md:flex'
+        />
+      </div>
+    </>
+  );
 };
 
 export default page;
