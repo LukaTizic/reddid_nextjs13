46b74dc57bf0122ecd64cbe2280d7b709a53b377
aluca94@gmail.com <alutza94@gmail.com>
2024-01-23 18:48:17 +0100

    implement picture, name, username

--- components/shared/cards/UserCard.tsx
+++ components/shared/cards/UserCard.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 interface Props {
   user: {
     _id: string;
@@ -9,7 +12,30 @@ interface Props {
 }
 
 const UserCard = ({ user }: Props) => {
-  return <div>UserCard</div>;
+  return (
+    <Link
+      href={`/profile/${user.clerkId}`}
+      className='shadow-light100_darknone w-full max-xs:min-w-full xs:w-[260px]'
+    >
+      <article className='background-light900_dark200 light-border flex w-full flex-col items-center justify-center rounded-2xl border p-8'>
+        <Image
+          src={user.picture}
+          alt='profile picture'
+          width={100}
+          height={100}
+          className='rounded-full'
+        />
+        <div className='mt-4 text-center'>
+          <h3 className='h3-bold text-dark200_light900 line-clamp-1'>
+            {user.name}
+          </h3>
+          <p className='body-regular text-dark500_light500 mt-2'>
+            @{user.username}
+          </p>
+        </div>
+      </article>
+    </Link>
+  );
 };
 
 export default UserCard;
