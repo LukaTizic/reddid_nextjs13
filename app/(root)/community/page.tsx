459a3fe753e2b9affe231569c0740e1dc8d0939e
aluca94@gmail.com <alutza94@gmail.com>
2024-01-23 18:41:28 +0100

    add UserCard

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -1,8 +1,13 @@
 import Filter from "@/components/shared/Filter";
+import UserCard from "@/components/shared/cards/UserCard";
 import LocalSearchBar from "@/components/shared/navbar/search/LocalSearchBar";
 import { UserFilters } from "@/constants/filters";
+import { getAllUsers } from "@/lib/actions/user.action";
+import Link from "next/link";
 
-const page = () => {
+const Page = async () => {
+  const result = await getAllUsers({});
+
   return (
     <>
       <h1 className='h1-bold text-dark100_light900'>All Users</h1>
@@ -21,9 +26,20 @@ const page = () => {
         />
       </div>
 
-      <section className='mt-12 flex flex-wrap gap-4 '></section>
+      <section className='mt-12 flex flex-wrap gap-4 '>
+        {result.users.length > 0 ? (
+          result.users.map((user) => <UserCard key={user._id} user={user} />)
+        ) : (
+          <div className='paragraph-regular text-dark200_light800 mx-auto max-w-4xl text-center'>
+            <p>No users found.</p>
+            <Link href='/sign-up' className='mt-2 font-bold text-accent-blue'>
+              Join our community!
+            </Link>
+          </div>
+        )}
+      </section>
     </>
   );
 };
 
-export default page;
+export default Page;
