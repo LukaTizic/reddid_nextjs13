5145a2fe9d10fb3dd2409da9982c401e59517974
aluca94@gmail.com <alutza94@gmail.com>
2024-01-24 13:21:57 +0100

    Add tags

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
+import UserCard from "@/components/shared/cards/UserCard";
+import LocalSearchBar from "@/components/shared/navbar/search/LocalSearchBar";
+import { UserFilters } from "@/constants/filters";
+import { getAllTags } from "@/lib/actions/tag.actions";
+import Link from "next/link";
 
-const page = () => {
-  return <div>page</div>;
+const Page = async () => {
+  const result = await getAllTags({});
+
+  return (
+    <>
+      <h1 className='h1-bold text-dark100_light900'>All Tags</h1>
+
+      <div className='mt-11 flex justify-between gap-5 max-sm:flex-col items-center'>
+        <LocalSearchBar
+          route='/tags'
+          iconPosition='left'
+          imgSrc='/assets/icons/search.svg'
+          placeholder='Search for unique tags'
+          otherClasses='flex-1'
+        />
+        <Filter
+          filters={UserFilters}
+          otherClasses='min-h-[56px] sm:min-w-[170px]'
+        />
+      </div>
+
+      <section className='mt-12 flex flex-wrap gap-4 '>
+        {result.tags.length > 0 ? (
+          result.tags.map((tag) => <div> Tag Card</div>)
+        ) : (
+          <NoResult
+            title='No Tags Found'
+            description='It looks like there are no tags found'
+            link='/ask-question'
+            linkTitle='Ask a question'
+          />
+        )}
+      </section>
+    </>
+  );
 };
 
-export default page;
+export default Page;
