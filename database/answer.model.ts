f00112d28e0fb4d94a82939d48e3880198eb98b5
aluca94@gmail.com <alutza94@gmail.com>
2024-01-25 15:55:48 +0100

    Implement AnswerSchema

--- database/answer.model.ts
+++ database/answer.model.ts
@@ -9,7 +9,14 @@ export interface IAnswer extends Document {
   createdAt: Date;
 }
 
-const AnswerSchema = new Schema({});
+const AnswerSchema = new Schema({
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
+  content: { type: String, required: true },
+  upvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  downvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  createdAt: { type: Date, default: Date.now },
+});
 
 const Answer = models.Answer || model("Answer", AnswerSchema);
 
