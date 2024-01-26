9d7f4712034c07b37ac1e58b6125e3b883b81eec
aluca94@gmail.com <alutza94@gmail.com>
2024-01-26 23:38:52 +0100

    #

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -12,16 +12,14 @@ export async function createAnswer(params: CreateAnswerParams) {
 
     const { content, author, question, path } = params;
 
-    const newAnswer = await Answer.create({
-      content,
-      author,
-      question,
-    });
+    const newAnswer = await Answer.create({ content, author, question });
 
-    // add answer to the questions array
+    // Add the answer to the question's answers array
     await Question.findByIdAndUpdate(question, {
       $push: { answers: newAnswer._id },
     });
+
+    // TODO: Add interaction...
 
     revalidatePath(path);
   } catch (error) {
@@ -35,6 +33,7 @@ export async function getAnswers(params: GetAnswersParams) {
     connectToDatabase();
 
     const { questionId } = params;
+
     const answers = await Answer.find({ question: questionId })
       .populate("author", "_id clerkId name picture")
       .sort({ createdAt: -1 });
