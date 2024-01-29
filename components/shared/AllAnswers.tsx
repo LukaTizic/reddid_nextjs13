b926e0397eef7fc8537c0ba3cc457022a4e7e864
aluca94@gmail.com <alutza94@gmail.com>
2024-01-29 12:12:13 +0100

    finish AllAnswer component

--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import Filter from "./Filter";
 import { AnswerFilters } from "@/constants/filters";
 import { getAnswers } from "@/lib/actions/answer.action";
-import Answer from "@/database/answer.model";
-import { Link } from "lucide-react";
+import Link from "next/link";
 import Image from "next/image";
 import { getTimestamp } from "@/lib/utils";
+import ParseHtml from "./ParseHtml";
 
 interface Props {
   questionId: string;
   userId: string;
   totalAnswers: number;
   page?: number;
-  filter?: string;
+  filter?: number;
 }
 
 const AllAnswers = async ({
@@ -22,16 +22,18 @@ const AllAnswers = async ({
   page,
   filter,
 }: Props) => {
-  const result = await getAnswers({ questionId });
+  const result = await getAnswers({
+    questionId,
+  });
 
   return (
     <div className='mt-11'>
       <div className='flex items-center justify-between'>
-        <h3 className='primary-text-gradient font-bold'>
-          {totalAnswers} Answers
-        </h3>
+        <h3 className='primary-text-gradient'>{totalAnswers} Answers</h3>
+
         <Filter filters={AnswerFilters} />
       </div>
+
       <div>
         {result.answers.map((answer) => (
           <article key={answer._id} className='light-border border-b py-10'>
@@ -49,15 +51,19 @@ const AllAnswers = async ({
                     className='rounded-full object-cover max-sm:mt-0.5'
                   />
                   <div className='flex flex-col sm:flex-row sm:items-center'>
-                    <p>{answer.author.name}</p>
-                    <p>
-                      <span className='max-sm:hidden'>-</span> answered{" "}
-                      {getTimestamp(answer.createdAt)}
+                    <p className='body-semibold text-dark300_light700'>
+                      {answer.author.name}
+                    </p>
+
+                    <p className='small-regular text-light400_light500 ml-0.5 mt-0.5 line-clamp-1'>
+                      answered {getTimestamp(answer.createdAt)}
                     </p>
                   </div>
                 </Link>
+                <div className='flex justify-end'>VOTING</div>
               </div>
             </div>
+            <ParseHtml data={answer.content} />
           </article>
         ))}
       </div>
