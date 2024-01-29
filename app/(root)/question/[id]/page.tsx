73ccad762a70e3dc1bb63c6fd9e2f7363a9fd91d
aluca94@gmail.com <alutza94@gmail.com>
2024-01-29 12:26:42 +0100

    Implement Typescript (interface)

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -3,6 +3,7 @@ import AllAnswers from "@/components/shared/AllAnswers";
 import Metric from "@/components/shared/Metric";
 import ParseHtml from "@/components/shared/ParseHtml";
 import RenderTag from "@/components/shared/RenderTag";
+import Votes from "@/components/shared/Votes";
 import { getQuestionById } from "@/lib/actions/question.action";
 import { getUserById } from "@/lib/actions/user.action";
 import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
@@ -41,7 +42,18 @@ const Page = async ({ params, searchParams }) => {
               {result.author.name}
             </p>
           </Link>
-          <div className='flex justify-end'>Vote</div>
+          <div className='flex justify-end'>
+            <Votes
+              type='question'
+              itemId={JSON.stringify(result._id)}
+              userId={JSON.stringify(mongoUser._id)}
+              upvotes={result.upvotes.length}
+              hasupVoted={result.upvotes.includes(mongoUser._id)}
+              downvotes={result.downvotes.length}
+              hasdownVotes={result.downvotes.includes(mongoUser._id)}
+              hasSaved={mongoUser?.saved.includes(result._id)}
+            />
+          </div>
         </div>
         <h2 className='h2-semibold text-dark200_light900 mt-3.5 w-full text-left'>
           {result.title}
--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,6 +1,24 @@
-import React from "react";
+interface Props {
+  type: string;
+  itemId: string;
+  userId: string;
+  upvotes: number;
+  hasupVoted: boolean;
+  downvotes: number;
+  hasdownVoted: boolean;
+  hasSaved?: boolean;
+}
 
-const Votes = () => {
+const Votes = ({
+  type,
+  itemId,
+  userId,
+  upvotes,
+  hasupVoted,
+  downvotes,
+  hasdownVoted,
+  hasSaved,
+}: Props) => {
   return <div>Votes</div>;
 };
 
