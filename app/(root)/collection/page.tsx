f90f7ac0cc9337816e895f82ceda7fe962ce5755
aluca94@gmail.com <alutza94@gmail.com>
2024-01-31 18:58:08 +0100

    finish community page

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
+import QuestionCard from "@/components/shared/cards/QuestionCard";
+import LocalSearchBar from "@/components/shared/navbar/search/LocalSearchBar";
+import { QuestionFilters } from "@/constants/filters";
+import { getSavedQuestions } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
 
-const page = () => {
-  return <div>page</div>;
-};
+export default async function Home() {
+  const { userId } = auth();
 
-export default page;
+  if (!userId) return null;
+
+  const result = await getSavedQuestions({
+    clerkId: userId,
+  });
+
+  return (
+    <>
+      <h1 className='h1-bold text-dark100_light900'>All Questions</h1>
+
+      <div className='mt-11 flex justify-between gap-5 max-sm:flex-col items-center'>
+        <LocalSearchBar
+          route='/'
+          iconPosition='left'
+          imgSrc='/assets/icons/search.svg'
+          placeholder='Search for questions'
+          otherClasses='flex-1'
+        />
+        <Filter
+          filters={QuestionFilters}
+          otherClasses='min-h-[56px] sm:min-w-[170px]'
+        />
+      </div>
+
+      <div className='mt-10 flex w-full flex-col gap-6'>
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There's no saved question to show"
+            description='Be the first to break the silence! Ask a Question and kickstart the
+        discussion.Our query could be the next big thing others learn from! Get
+        involved and help!'
+            link='/ask-question'
+            linkTitle='Ask a Question'
+          />
+        )}
+      </div>
+    </>
+  );
+}
