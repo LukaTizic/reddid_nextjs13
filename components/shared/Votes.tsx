db1f9c4e1acaaf64c29a0e4a69ed34c28def1b16
aluca94@gmail.com <alutza94@gmail.com>
2024-01-31 18:33:20 +0100

    Implement handleSave()

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -5,9 +5,11 @@ import {
   downvoteQuestion,
   upvoteQuestion,
 } from "@/lib/actions/question.action";
+import { toggleSaveQuestion } from "@/lib/actions/user.action";
 import { formatAndDivideNumber } from "@/lib/utils";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
+import { type } from "os";
 
 interface Props {
   type: string;
@@ -33,7 +35,13 @@ const Votes = ({
   const pathname = usePathname();
   const router = useRouter();
 
-  const handleSave = () => {};
+  const handleSave = async () => {
+    await toggleSaveQuestion({
+      userId: JSON.parse(userId),
+      questionId: JSON.parse(itemId),
+      path: pathname,
+    });
+  };
 
   const handleVote = async (action: string) => {
     if (!userId) {
