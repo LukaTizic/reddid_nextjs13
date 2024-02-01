cdabb32d9df6cec4e065d27e202a46900d2fb076
aluca94@gmail.com <alutza94@gmail.com>
2024-02-01 12:49:16 +0100

    Finigsh tag details

--- app/(root)/tags/[id]/page.tsx
+++ app/(root)/tags/[id]/page.tsx
@@ -1,7 +1,62 @@
+import NoResult from "@/components/shared/NoResult";
+import QuestionCard from "@/components/shared/cards/QuestionCard";
+import LocalSearchBar from "@/components/shared/navbar/search/LocalSearchBar";
+import { IQuestions } from "@/database/question.model";
+import { getQuestionsByTagId } from "@/lib/actions/tag.actions";
+import { URLProps } from "@/types";
 import React from "react";
 
-const page = ({ params, searchParams }) => {
-  return <div>{params.id}</div>;
+const Page = async ({ params, searchParams }: URLProps) => {
+  const result = await getQuestionsByTagId({
+    tagId: params.id,
+    page: 1,
+    searchQuery: searchParams.q,
+  });
+
+  return (
+    <>
+      <h1 className='h1-bold text-dark100_light900 capitalize'>
+        {result.tagTitle}
+      </h1>
+
+      <div className='mt-11 w-full'>
+        <LocalSearchBar
+          route='/'
+          iconPosition='left'
+          imgSrc='/assets/icons/search.svg'
+          placeholder='Search for tag questions'
+          otherClasses='flex-1'
+        />
+      </div>
+
+      <div className='mt-10 flex w-full flex-col gap-6'>
+        {result.questions.length > 0 ? (
+          result.questions.map((question: IQuestions) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There's no tag question to show"
+            description='Be the first to break the silence! Ask a Question and kickstart the
+        discussion.Our query could be the next big thing others learn from! Get
+        involved and help!'
+            link='/ask-question'
+            linkTitle='Ask a Question'
+          />
+        )}
+      </div>
+    </>
+  );
 };
 
-export default page;
+export default Page;
