3b74ae3414d4da79fdeac7de38456b0b1adae07c
aluca94@gmail.com <alutza94@gmail.com>
2024-02-12 12:41:54 +0100

    AnswersTab / Props - Typescript interface

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -90,7 +90,11 @@ const Page = async ({ params, searchParams }: URLProps) => {
             />
           </TabsContent>
           <TabsContent value='answers'>
-            <AnswersTab />
+            <AnswersTab
+              searchParams={searchParams}
+              userId={userInfo.user._id}
+              clerkId={clerkId}
+            />
           </TabsContent>
         </Tabs>
       </div>
--- components/shared/AnswersTab.tsx
+++ components/shared/AnswersTab.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import { SearchParamsProps } from "@/types";
 
-const AnswersTab = () => {
+interface Props extends SearchParamsProps {
+  userId: string;
+  clerkId?: string | null;
+}
+const AnswersTab = async ({ searchParams, userId, clerkId }: Props) => {
   return <div>AnswersTab</div>;
 };
 
